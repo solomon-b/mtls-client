4f388950c334b8a24425216d82e355e44b237352
solomon <ssbothwell@gmail.com>
2023-01-10 15:59:40 -0800

    initial commit

---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+
+  description = "mTLS Client Example";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
+
+    flake-utils = {
+      url = github:numtide/flake-utils;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    let
+      ghcVersion = "924";
+      compiler = "ghc${ghcVersion}";
+      # default systems compatible with pre-commit-hooks.nix
+      # https://github.com/cachix/pre-commit-hooks.nix/pull/122
+      defaultSystems = [
+        "aarch64-linux"
+        # "aarch64-darwin"
+        "i686-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+    in
+    flake-utils.lib.eachSystem defaultSystems (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        # need to do the evalPkgs trick so that IFD works with `nix flake check`
+        # https://github.com/NixOS/nix/issues/4265
+        evalPkgs = import nixpkgs { system = "x86_64-linux"; };
+
+        # Our haskell packages override, needs to use evalPkgs because
+        # cabal2nix uses IFD
+        hsPkgs = evalPkgs.haskell.packages.${compiler}.override {
+          overrides = hfinal: hprev: {
+            mtls-client = hfinal.callCabal2nix "mtls-client" ./. { };
+          };
+        };
+      in
+      rec {
+
+        # Note: cannot reference anything that depends on `evalPkgs` like `hsPkgs`
+        # otherwise non-x86_64-linux users will not be able to build the dev env
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cabal2nix
+            cabal-install
+            ghcid
+            haskell.compiler.${compiler}
+            haskell.packages.${compiler}.haskell-language-server
+            ormolu
+            zlib
+          ];
+        };
+
+        packages = flake-utils.lib.flattenTree {
+          mtls-client = hsPkgs.mtls-client;
+        };
+
+        defaultPackage = packages.mtls-client;
+      });
+}
