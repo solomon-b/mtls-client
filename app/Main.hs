ba6bf7506bb37647f3e794cfe186cec7172f44e6
solomon <ssbothwell@gmail.com>
2023-01-10 16:35:30 -0800

    Basic HTTPS GET request

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,44 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+--------------------------------------------------------------------------------
+
+import Data.Default (Default (..))
+import Network.Connection (TLSSettings (..))
+import Network.HTTP.Client (Response (..), httpLbs, parseRequest)
+import Network.HTTP.Client.TLS (mkManagerSettings, newTlsManagerWith)
+import Network.HTTP.Types.Status (statusCode)
+import Network.TLS (ClientParams (..), Shared (..), Supported (..))
+import Network.TLS.Extra (ciphersuite_strong)
+import System.X509 (getSystemCertificateStore)
+
+--------------------------------------------------------------------------------
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  store <- getSystemCertificateStore
+
+  let tlsSettings =
+        TLSSettings $
+          ClientParams
+            { clientUseMaxFragmentLength = Nothing,
+              clientServerIdentification = ("httpbin.org", ""),
+              clientUseServerNameIndication = True,
+              clientWantSessionResume = def,
+              clientShared = def {sharedCAStore = store},
+              clientHooks = def,
+              clientSupported = def {supportedCiphers = ciphersuite_strong},
+              clientDebug = def,
+              clientEarlyData = def
+            }
+
+  let managerSettings = mkManagerSettings tlsSettings Nothing
+  manager <- newTlsManagerWith managerSettings
+
+  initialRequest <- parseRequest "https://httpbin.org/get" -- "https://client.badssl.com/"
+  let request = initialRequest
+
+  response <- httpLbs request manager
+  print $ "The status code was: " ++ show (statusCode $ responseStatus response)
+  print $ responseBody response
